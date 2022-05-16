6a33bf59fbba73eb4069e46e04b1a177c2b079f0
josemarticorena <j.marticorena@bde.com.gt>
2022-05-16 14:29:39 -0600

    com

---
+++ Primeros_Pasos/MisTags.py
@@ -0,0 +1,20 @@
+from calendar import formatstring
+from pylogix import PLC 
+
+IPAdd = '192.168.11.1'  #ESCRIBIR DIRECCION IP DEL PLC
+Slot = 2    #ESCRIBIR EL SLOT DEL PROCESADOR (EN CONTROL LOGIX PUEDE SER DISTINTO DE 0)
+
+
+ControlLogix = PLC()
+ControlLogix.IPAddress = IPAdd
+ControlLogix.ProcessorSlot = Slot
+
+Alltags = ControlLogix.GetTagList()
+
+#Al no estar conectada a un PLC no funciona
+with open('tag_list2.txt', 'w') as f:
+    for t in Alltags.Value:
+        f.write('\n' + t.TagName )
+#print(Alltags.Value)
+for t in Alltags.Value:
+    print(t.TagName)
