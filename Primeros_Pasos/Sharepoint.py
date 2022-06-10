7bdf669400dcaf6b11c855b7649514f662aa0b4d
josemarticorena <j.marticorena@bde.com.gt>
2022-06-10 02:16:09 -0400

    nuevo

--- Primeros_Pasos/Sharepoint.py
+++ Primeros_Pasos/Sharepoint.py
@@ -5,4 +5,9 @@ from shareplum.site import Version
 authcookie = Office365('https://ingenieriausacedu.sharepoint.com', username='3003552890101@ingenieria.usac.edu.gt', password='Corenamarti498').GetCookies()
 site = Site('https://ingenieriausacedu.sharepoint.com/sites/EnlacePLC/', authcookie=authcookie)
 
-site.AddList('My New List2', description='Great List!', template_id='Custom List')
+#site.AddList('Eficiency list', description='Great List!', template_id='Custom List')
+
+new_list = site.List('Eficiency list')
+update_data = [{'ID': '1', 'Tiempo_Total_Paro': '60'},
+               {'ID': '2', 'Title': 'Another Change'}]
+new_list.UpdateListItems(data=update_data, kind='Update')
